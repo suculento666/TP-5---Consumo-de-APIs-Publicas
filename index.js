939666c699208101fb7a98496cd08badd7591d9b
FIDEL GOLDSMAN <49002222@ort>
2026-04-10 09:51:08 -0300

    pa tras fren

---
+++ index.js
@@ -0,0 +1,7 @@
+import express from 'express'; 
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+const PORT = 3000;
+const POKEAPI_BASE = 'https://pokeapi.co/api/v2';
